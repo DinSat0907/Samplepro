f4db92bff64a665ce7f69ceb0ee0047c8050a5d8
DinSat0907 <dineshkumar@gmail.co.in>
2024-04-11 01:54:41 +0530

    Created the Login Page and Home Page and Modified the App.js

--- App.js
+++ App.js
@@ -1,16 +1,108 @@
-import React from 'react';
-import Click from './onClick';
+import React, { useState } from 'react'
+import { View, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native'
 
-export default function App(props){
+export default function Login(){
+
+  const [changeName, setChangeName] = useState('')
+  const [changeNameError, setChangeNameError] = useState(false)
   
-  const pop = ()=>{
-    alert("Don't touch me")
+  const HandleNameChange = (e)=>{
+    setChangeName(e)
+    console.log(e)
+    if (e != '') {
+
+      setChangeNameError(true)
+
+    }
+    else{
+
+      setChangeNameError(false)
+
+    }
   }
-  
-  return(
+
+  return (
+    <View style={Styles.LogCon}>
+      <View style={Styles.Header}>
+        <Text style={Styles.LogName}>Welcome to Login Page</Text>
+      </View>
+      <View style={Styles.InputCon}>
+       <View style={Styles.NameInput}>
+        <TextInput keyboardType='default' placeholder='User Name'maxLength={15} value={changeNameError} onChangeText={(e)=>HandleNameChange(e)}></TextInput>
+       </View>
+       <View style={Styles.MobileInput}>
+        <TextInput keyboardType='number-pad' placeholder='Mobile Number' maxLength={10}></TextInput>
+       </View>
+       <View style={Styles.PasswordInput}>
+        <TextInput keyboardType = 'numeric' placeholder='Password' maxLength={5}></TextInput>
+       </View>
+       <Text>{changeName}</Text>
+        <TouchableOpacity style={Styles.SubmitBut}>
+         <Text style={{color: 'white'}}>LogIn</Text>
+        </TouchableOpacity>
+      </View>      
+    </View>
     
-      <Click handleClick= {pop} />
-   
-  
-    )
+  )
 }
+
+const Styles = StyleSheet.create({
+  LogCon: {
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flex: 1,
+    display: 'flex', 
+    backgroundColor: 'orange', 
+    fontFamily: 'serif'
+  },
+
+  LogName: {
+    fontSize: 30,
+    fontWeight: '900',
+    marginTop: 50
+  },
+  
+  NameInput: {
+    borderWidth: 2,
+    borderColor: 'gray',
+    borderRadius: 10,
+    alignItems: 'center',
+    height: 40,
+    width: 220,
+    backgroundColor: 'lightgray'
+    },
+
+  MobileInput: {
+    borderWidth: 2,
+    borderColor: 'gray',
+    borderRadius: 10,
+    marginTop: 9,
+    alignItems: 'center',
+    height: 40,
+    width: 220,
+    backgroundColor: 'lightgray'
+    },
+  
+  PasswordInput: {
+    borderWidth: 2,
+    borderColor: 'gray',
+    borderRadius: 10,
+    marginTop: 9,
+    alignItems: 'center',
+    height: 40,
+    width: 220,
+    backgroundColor: 'lightgray'
+  },
+
+  SubmitBut: {
+    borderWidth: 2,
+    borderColor: '#cccccc',
+    borderRadius: 10,
+    marginTop: 60,
+    paddingVertical: 10,
+    alignItems: 'center',
+    height: 40,
+    width: 220,
+    backgroundColor: 'blue',
+  }
+})
---
+++ Home.js
@@ -0,0 +1,15 @@
+// import React from "react"
+// import { View, Text, Button, StyleSheet } from 'react-native';
+
+
+
+// export default function Home(){
+    
+//     return(
+    
+//     <View styles={Styles.HomeCon}>
+//       <Text>Welcome to my WebPage</Text>
+//       <Button title="Go back to Login"/>
+//     </View>
+//     )
+// }

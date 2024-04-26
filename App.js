d268971a4275f2fc3ab2b414fd4e58cb8b5ed251
DinSat0907 <dineshkumar@gmail.co.in>
2024-04-26 19:07:12 +0530

    Implemented Button tag in App.js

--- App.js
+++ App.js
@@ -1,24 +1,37 @@
 import React, { useEffect, useState} from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import axios from 'axios';
 
 export default function APP(){
   const url = "https://reqres.in/api/users?page=2";
   const [data, setData] = useState([]);
 
-  const fetchInfo = () => {
-    return axios.get(url).then((res) => setData(res.data.data));
-  }
 
-  useEffect(() => {
-    fetchInfo();
-  }, []);
+  // useEffect(() => {
+  //   fetchInfo();
+  // }, []);
+
+  const fetchInfo = async() => {
+    // return axios.get(url).then((res) => setData(res.data.data));
+
+      try {
+        const response = await axios.get(url);
+        setData(response.data.data)
+  
+        return response;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+      }
+    
+  }
 
     return (
       <View>
         <View style={styles.container}>
-        <Text style={styles.text1}>ID</Text><Text style={styles.text1}>Email</Text><Text style={styles.text1}>First_Name</Text><Text style={styles.text1}>Last_Name</Text><Text style={styles.text1}>Avatar</Text>
+        <Text style={styles.text1}>Id</Text><Text style={styles.text1}>Email</Text><Text style={styles.text1}>First_Name</Text><Text style={styles.text1}>Last_Name</Text><Text style={styles.text1}>Avatar</Text>
         </View>
+        
         {data.map((u)=>{
           return ( 
             <View style={styles.container}>      
@@ -27,9 +40,8 @@ export default function APP(){
           )
         }
       )}  
+      <Button title='Get Data' onPress={fetchInfo}></Button>
       </View>
-      
-
     )
 }
 
@@ -39,7 +51,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flexDirection: 'row',
     justifyContent:'space-around',
-    backgroundColor: 'orange',
     marginLeft:45
   },
   text1:{
@@ -50,16 +61,19 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: 'black',
     fontWeight: 700,
-    fontSize: 20
+    fontSize: 20,
+    height: 100,
+    backgroundColor: 'orange',
   },
   text2:{
     textAlign: 'center',
     flex:1,
     width: 280,
-    paddingTop:50,
+    paddingTop:45,
     borderWidth: 1,
     borderColor: 'black',
-    fontSize: 15
+    fontSize: 15,
+    backgroundColor: 'orange',
   }
 })
 

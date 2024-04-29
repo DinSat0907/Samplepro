003306cd10b980095f41fe3d81ba59aeb5b41b9d
DinSat0907 <dineshkumar@gmail.co.in>
2024-04-29 15:07:29 +0530

    Implemented the POST Method in App.js

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useState} from 'react'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import axios from 'axios';
+var id = 13
 
 export default function APP(){
   const url = "https://reqres.in/api/users?page=2";
   const [data, setData] = useState([]);
+  const [first_name, setFirst_Name] = useState('')
 
 
-  // useEffect(() => {
-  //   fetchInfo();
-  // }, []);
+  useEffect(() => {
+    fetchInfo();
+  }, []);
 
   const fetchInfo = async() => {
     // return axios.get(url).then((res) => setData(res.data.data));
@@ -17,65 +19,87 @@ export default function APP(){
       try {
         const response = await axios.get(url);
         setData(response.data.data)
-  
         return response;
       } catch (error) {
         console.error('Error fetching data:', error);
         throw error;
       }
+  }
+
+
+  const postUser = async()=>{
+    try {
+      const response = await axios.post(url,{id:id++,first_name: first_name});
+      setData([...data,response.data])
+      console.log([...data,response.data])
+      // setFirst_Name('')
+
+      return response;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      throw error;
+    }
     
+
   }
 
     return (
       <View>
         <View style={styles.container}>
-        <Text style={styles.text1}>Id</Text><Text style={styles.text1}>Email</Text><Text style={styles.text1}>First_Name</Text><Text style={styles.text1}>Last_Name</Text><Text style={styles.text1}>Avatar</Text>
+        <Text style={styles.text1}>First_Name</Text>
         </View>
         
         {data.map((u)=>{
           return ( 
-            <View style={styles.container}>      
-              <Text style={styles.text2}>{u.id}</Text><Text style={styles.text2}>{u.email}</Text><Text style={styles.text2}>{u.first_name}</Text><Text style={styles.text2}>{u.last_name}</Text><Text style={styles.text2}>{u.avatar}</Text>
+            <View key={u.id} style={styles.container}>      
+              <Text  style={styles.text2}>{u.first_name}</Text>
             </View>  
           )
         }
-      )}  
-      <TouchableOpacity style={styles.btn} onPress={fetchInfo}>Get Data</TouchableOpacity>
+      )}
+      <View style={styles.postContainer}>
+      <TextInput style={styles.inputField} placeholder='Post Name' value={first_name} onChangeText={e=>setFirst_Name(e)}></TextInput>
+      
+      <TouchableOpacity style={styles.btn} onPress={postUser}>Post</TouchableOpacity>
+      </View>  
+
+      {/* <TouchableOpacity style={styles.btn} onPress={fetchInfo}>Update</TouchableOpacity>
+      <TouchableOpacity style={styles.btn} onPress={fetchInfo}>Delete</TouchableOpacity> */}
       </View>
     )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     justifyContent: 'center',
     flexDirection: 'row',
-    justifyContent:'space-around',
-    marginLeft:45
+    justifyContent:'space-between',
+    width: 500,
+    // marginLeft:445
   },
   text1:{
     textAlign: 'center',
     flex:1,
     width: 280,
-    paddingTop:40,
+    paddingTop:15,
     borderWidth: 1,
     borderColor: 'black',
     fontWeight: 700,
     fontSize: 20,
-    height: 100,
+    height: 50,
     backgroundColor: 'orange',
   },
   text2:{
     textAlign: 'center',
     flex:1,
     width: 280,
-    paddingTop:45,
     borderWidth: 1,
     borderColor: 'black',
     fontSize: 15,
     backgroundColor: 'orange',
   },
   btn: {
+    flex:1,
     alignSelf: 'center',
     paddingTop:10,
     textAlign: 'center',
@@ -83,7 +107,21 @@ const styles = StyleSheet.create({
     height: 40,
     backgroundColor: 'blue',
     borderRadius: 8,
-    marginTop: 10
+    marginLeft: 10
+
+  },
+  inputField:{
+    flex:1,
+    textAlign:'center',
+    borderRadius: 8,
+    height:40,
+    borderWidth: 1
+  },
+  postContainer:{
+    paddingTop:10,
+    alignSelf:'center',
+    flexDirection:'row',
+    justifyContent: 'space-around'
   }
 })
 
